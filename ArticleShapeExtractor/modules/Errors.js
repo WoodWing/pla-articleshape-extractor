8931d178fb1d4d0cfa4caff7c80937caa45717b0
Edwin van der Klaauw <ekl@woodwing.com>
2025-04-17 16:09:35 +0200

    LA-91 Make the error classes globally accessible. Should have been part of commit 6bd276e180f003cc48439e9434ec929ebf3a4bdf.

--- ArticleShapeExtractor/modules/Errors.js
+++ ArticleShapeExtractor/modules/Errors.js
@@ -4,51 +4,68 @@ class ArgumentError extends Error {
         this.name = this.constructor.name;
     }
 }
+globalThis.ArgumentError = ArgumentError;
+
 class ConfigurationError extends Error {
     constructor(message) {
         super(`Invalid configuration. ${message}`);
         this.name = this.constructor.name;
     }
 }
+globalThis.ConfigurationError = ConfigurationError;
+
 class NoStudioSessionError extends Error {
     constructor() {
         super("Not logged in to WoodWing Studio.");
         this.name = this.constructor.name;
     }
 }
+globalThis.NoStudioSessionError = NoStudioSessionError;
+
 class NoDocumentOpenedError extends Error {
     constructor() {
         super("No document opened.");
         this.name = this.constructor.name;
     }
 }
+globalThis.NoDocumentOpenedError = NoDocumentOpenedError;
+
 class NoFramesSelectedError extends Error {
     constructor() {
         super("No frames selected.");
         this.name = this.constructor.name;
     }
 }
+globalThis.NoFramesSelectedError = NoFramesSelectedError;
+
 class NoTextOrGraphicalFramesSelectedError extends Error {
     constructor() {
         super("No text/graphical frame selected.");
         this.name = this.constructor.name;
     }
 }
+globalThis.NoTextOrGraphicalFramesSelectedError = NoTextOrGraphicalFramesSelectedError;
+
 class NoArticlesInDocumentError extends Error {
     constructor() {
         super("No articles found in the document.");
         this.name = this.constructor.name;
     }
 }
+globalThis.NoArticlesInDocumentError = NoArticlesInDocumentError;
+
 class NoFolderSelectedError extends Error {
     constructor() {
         super("No folder selected.");
         this.name = this.constructor.name;
     }
 }
+globalThis.NoFolderSelectedError = NoFolderSelectedError;
+
 class PrintLayoutAutomationError extends Error {
     constructor() {
         super(message);
         this.name = this.constructor.name;
     }
 }
+globalThis.PrintLayoutAutomationError = PrintLayoutAutomationError;
