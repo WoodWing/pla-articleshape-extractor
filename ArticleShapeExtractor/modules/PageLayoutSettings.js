WoodWing/pla-articleshape-extractor

commit 1cc90c8efd9428cadec441cfb1bf6d96ce5ee9b7

LA-117 Prevent stack whole dump in logfile on layout settings configuration error. Avoid processing a layout with mismatching settings.

ArticleShapeExtractor/modules/PageLayoutSettings.js
@@ -12,7 +12,16 @@ const formats = require('uxp').storage.formats;
 function PageLayoutSettings(logger) {
     this._logger = logger;
 
+    /**
+     * Exports the layout settings of the given layout document to a file named
+     * "_manifest/page-layout-settings.json" in the given folder. When this file 
+     * already exists, the settings are compared instead.
+     * @param {Document} doc 
+     * @param {Folder} folder 
+     * @returns {boolean} True when the settings are matching (or new), false otherwise.
+     */
     this.exportSettings = async function(doc, folder) {
+        const exportedSuccessfully = false;
         const docName = doc.saved ? lfs.getNativePath(await doc.fullName) : doc.name;
         this._logger.info("Exporting Document Settings for layout '{}'.", docName);
         app.scriptPreferences.measurementUnit = idd.MeasurementUnits.POINTS;
@@ -25,12 +34,17 @@ function PageLayoutSettings(logger) {
             const { inside, outside } = this._getInsideOutsideMargins(doc, page);
             const settings = this._composeSettings(doc, page, inside, outside);
             await this._saveOrComparePageLayoutSettings(settings, folder);
+            exportedSuccessfully = true;
         } catch(error) {
-            this._logger.logError(error);
+            const { ConfigurationError } = require('./Errors.js');
+            if (!(error instanceof ConfigurationError)) {
+                this._logger.logError(error);
+            }
             alert("An error occurred: " + error.message);
         } finally {
             app.scriptPreferences.measurementUnit = idd.AutoEnum.AUTO_VALUE;
         }
+        return exportedSuccessfully;
     }
 
     /**
@@ -106,6 +120,7 @@ function PageLayoutSettings(logger) {
 
     /**
      * Save a settings object to the "_manifest/page-layout-settings.json" file in a provided export folder.
+     * If the file already exists, it compares whether the given settings are equal with settings from the file.
      * @param {Object} settings
      * @param {Folder} exportFolder
      */

ArticleShapeExtractor/modules/ExportInDesignArticlesToFolder.js
@@ -28,7 +28,9 @@ function ExportInDesignArticlesToFolder(
      * @returns {Number} Count of exported article shapes.
      */
     this.run = async function(doc, folder) {
-        await this._pageLayoutSettings.exportSettings(doc, folder);
+        if (!(await this._pageLayoutSettings.exportSettings(doc, folder))) {
+            return 0;
+        }
 
         const lfs = require('uxp').storage.localFileSystem;
         const docName = doc.saved ? lfs.getNativePath(await doc.fullName) : doc.name;

commit 80357e8609f515e4679ba5d64e0da07a40762ad2

LA-117 Fix problem introduced in commit 1cc90c8efd9428cadec441cfb1bf6d96ce5ee9b7, attempt to change the value of a const.

ArticleShapeExtractor/modules/PageLayoutSettings.js
@@ -21,7 +21,7 @@ function PageLayoutSettings(logger) {
      * @returns {boolean} True when the settings are matching (or new), false otherwise.
      */
     this.exportSettings = async function(doc, folder) {
-        const exportedSuccessfully = false;
+        let exportedSuccessfully = false;
         const docName = doc.saved ? lfs.getNativePath(await doc.fullName) : doc.name;
         this._logger.info("Exporting Document Settings for layout '{}'.", docName);
         app.scriptPreferences.measurementUnit = idd.MeasurementUnits.POINTS;