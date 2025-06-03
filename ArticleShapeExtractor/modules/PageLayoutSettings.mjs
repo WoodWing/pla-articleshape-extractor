bec67ee7b2cf25b7910df6f1358010bb1a136e38
Edwin van der Klaauw <ekl@woodwing.com>
2025-06-03 15:29:37 +0200

    LA-117 Should have been part of commit b731f307c72a9ff41448bb0513d3fa5273713aaf

--- ArticleShapeExtractor/modules/PageLayoutSettings.mjs
+++ ArticleShapeExtractor/modules/PageLayoutSettings.mjs
@@ -165,34 +165,6 @@ class PageLayoutSettings{
     }
 
     /**
-     * Creates a subfolder under a given parent folder. Returns the subfolder if already exists.
-     * @param {Folder} parentFolder
-     * @param {string} subfolderName
-     * @returns {{entry: Folder, created: boolean}}
-     */
-    async #getOrCreateSubFolder(parentFolder, subfolderName) {
-        try {
-            return {entry: await parentFolder.getEntry(subfolderName), created: false};
-        } catch (e) {
-            return {entry: await parentFolder.createFolder(subfolderName, { overwrite: false }), created: true};
-        }
-    }
-
-    /**
-     * Creates a file in a given folder. Returns the file if already exists.
-     * @param {Folder} folder 
-     * @param {string} filename 
-     * @returns {{entry: File, created: boolean}}
-     */
-    async #getOrCreateFile(folder, filename) {
-        try {
-            return {entry: await folder.getEntry(filename), created: false};
-        } catch (e) {
-            return {entry: await folder.createFile(filename, { overwrite: false }), created: true};
-        }
-    }
-
-    /**
      * Compares two items, such as two objects and all their properties.
      * @param {any} lhs 
      * @param {any} rhs 
