99c1e17ec7ab866de4b36a17a9060a97b677e842
Edwin van der Klaauw <ekl@woodwing.com>
2025-12-05 11:01:01 +0100

    LA-187 Only compare the columns gutter and baseline grid increment of the page layout settings retrieved from local disk and from PLA service. Other properties may differ and won't be much of a problem when placing articles on layouts with mismatching page margins or dimensions.

---
+++ ArticleShapeUploader/modules/PageLayoutSettings.mjs
@@ -0,0 +1,103 @@
+export class PageLayoutSettings {
+
+    /** @type {{columnCount: number, rowCount: number}} */
+    #pageGrid;
+
+    /** @type {Object} */
+    #settings;
+    
+    /**
+     * 
+     * @param {Object} settings 
+     * @param {{columnCount: number, rowCount: number}} pageGrid 
+     */
+    constructor(settings, pageGrid) {
+        this.#pageGrid = pageGrid;
+        this.#settings = settings;
+    }
+
+    /**
+     * @param {{columnCount: number, rowCount: number}} pageGrid 
+     */
+    setPageGrid(pageGrid) {
+        this.#pageGrid = pageGrid;
+    }
+
+    /**
+     * The width of one column in points. The column refers to the simple page grid configured for PLA.
+     * @returns {number} Positive number.
+     */
+    getColumnWidth() {
+        const columnCount = this.#pageGrid.columnCount;
+        if (columnCount <= 0) {
+            throw new Error(`The column count ${columnWidth} is invalid.`);
+        }
+        const gutterCount = columnCount - 1;
+        const gutterWidth = this.#settings.columns.gutter;
+        const sumOfGuttersWidth = gutterWidth * gutterCount;
+        const columnWidth = (this.#getUsablePageWidth() - sumOfGuttersWidth) / columnCount;
+        if (columnWidth <= 0) {
+            throw new Error(`The column width ${columnWidth} is invalid.`);
+        }
+        return columnWidth;
+    }
+
+    /**
+     * The gutter space (in points) between the columns.
+     * @returns {number}
+     */
+    getColumnGutter() {
+        return this.#settings.columns.gutter;
+    }
+
+    /**
+     * @returns {Object}
+     */
+    getPageLayoutSettings() {
+        return this.#settings;
+    }
+
+    /**
+     * The width of the usable space within the page borders, in points.
+     * @returns {number}
+     */
+    #getUsablePageWidth() {
+        const pageWidth = this.#settings.dimensions.width;
+        const margins = this.#settings.margins;
+        return pageWidth - margins.inside - margins.outside;
+    }
+
+    /**
+     * The border width in point of the inner margin of a page.
+     * @returns {number} Right margin of a LHS page or left margin of a RHS page.
+     */
+    getPageMarginInside() {
+        return this.#settings.margins.inside;
+    }
+
+    /**
+     * The height of one row in points. The row refers to the simple page grid configured for PLA.
+     * @returns {number} Positive number.
+     */
+    getRowHeight() {
+        const rowCount = this.#pageGrid.rowCount;
+        if (rowCount <= 0) {
+            throw new Error(`The row count ${rowCount} is invalid.`);
+        }
+        const rowHeight = this.#getUsablePageHeight() / rowCount;
+        if (rowHeight <= 0) {
+            throw new Error(`The row height ${rowHeight} is invalid.`);
+        }
+        return rowHeight;
+    }
+
+    /**
+     * The height of the usable space in points. Usable space is the page area where content can be placed.
+     * The height of this area is the space between the page top margin and the page bottom margin.
+     * @returns {number}
+     */
+    #getUsablePageHeight() {
+        const settings = this.#settings;
+        return settings.dimensions.height - settings.margins.top - settings.margins.bottom;
+    }  
+}
